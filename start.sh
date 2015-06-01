22def9d6bebcbc81d7df73771184ef41fbc6dc80
redleader36 <redleader36@gmail.com>
2015-06-01 12:44:06 -0500

    changed date and directory structure

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@
 #               SETTINGS          
 L=''            #google login or email
 P=''            #google password
-AREA='https://www.ingress.com/intel?ll=35.682398,139.693909&z=11' #link to your location
+AREA='https://www.ingress.com/intel?ll=41.582589,-93.614026&z=14' #link to your location
 MINLEVEL='1'    #minimal portal level, set to 1 to display all available portals
 MAXLEVEL='8'    #highest portal level, set to 8 to display all
-V='120'          #Delay between capturing screenshots in seconds
-WIDTH='900'     #Picture width
-HEIGHT='500'    #Picture height
-FOLDER='./'     #Folder where to save screenshots with \ (or /) in the end. '.' means current folder. 
+V='150'          #Delay between capturing screenshots in seconds
+WIDTH='1280'     #Picture width
+HEIGHT='720'    #Picture height
+FOLDER='./images/'     #Folder where to save screenshots with \ (or /) in the end. '.' means current folder. 
 NUMBER='0'      #Number of screenshots to take. 0 for infinity.
 LOGLEVEL='3'    #Log level. 0 = silence, 1 = only short welcome, two-factor code and errors, 2 = everything from 1 + every screenshot announced, 3 = beautiful welcome + everything from 2, 4 = all logs including debug messages, short welcome
 #DO NOT EDIT ANYTHING BELOW THIS LINE
@@ -23,4 +23,4 @@ do
     fi
 done
 
-exec ./phantomjs ice.js $ARGS
+exec phantomjs ice.js $ARGS
